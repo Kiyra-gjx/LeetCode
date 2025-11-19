0da417f97abcec59e27a1924b3cbcc3c42410537
Kiyra <Kiyra.gjx@gmail.com>
2025-11-19 12:49:40 +0800

    t145对链表进行插入排序 方法二：分治-自顶向下

--- Java/leetcode/src/hot100/t148sortList.java
+++ Java/leetcode/src/hot100/t148sortList.java
@@ -25,21 +25,63 @@ public class t148sortList {
     }
     static class Solution {
         public ListNode sortList(ListNode head) {
-            PriorityQueue<ListNode> pq = new PriorityQueue<>((a, b) -> a.val - b.val);
-            ListNode curr = head;
-            while(curr != null) {
-                pq.add(curr);
-                curr = curr.next;
+            return sortList(head, null);
+        }
+
+        public ListNode sortList(ListNode head, ListNode tail) {
+            if (head == null) return head;
+            if (head.next == tail) {
+                head.next = null;
+                return head;
             }
+            ListNode slow = head, fast = head;
+            while(fast != tail) {
+                slow = slow.next;
+                fast = fast.next;
+                if (fast != tail) {
+                    fast = fast.next;
+                }
+            }
+            ListNode mid = slow;
+            ListNode list1 = sortList(head, mid);
+            ListNode list2 = sortList(mid, tail);
+            return merge(list1, list2);
+        }
+
+        public ListNode merge(ListNode head1, ListNode head2) {
             ListNode dummy = new ListNode(-1);
-            curr = dummy;
-            while (!pq.isEmpty()) {
-                ListNode node = pq.poll();
-                curr.next = node;
-                curr = curr.next;
-                curr.next = null;
+            ListNode curr = dummy;
+            ListNode ptr1 = head1, ptr2 = head2;
+            while(ptr1 != null || ptr2 != null) {
+                if (ptr1 == null) {
+                    curr.next = ptr2;
+                    break;
+                } else if (ptr2 == null) {
+                    curr.next = ptr1;
+                    break;
+                } else {
+                    if (ptr1.val <= ptr2.val) {
+                        curr.next = ptr1;
+                        ptr1 = ptr1.next;
+                    } else {
+                        curr.next = ptr2;
+                        ptr2 = ptr2.next;
+                    }
+                    curr = curr.next;
+                }
             }
             return dummy.next;
         }
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(4);
+        head.next = new ListNode(1);
+        head.next.next = new ListNode(2);
+        head.next.next.next = new ListNode(3);
+
+        Solution sol = new Solution();
+
+        System.out.println(sol.sortList(head));
     }
 }
