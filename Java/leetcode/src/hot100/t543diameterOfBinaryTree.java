58c85303c22974dfe71a404967ea3a22d94df1a7
Kiyra <Kiyra.gjx@gmail.com>
2025-11-22 18:34:10 +0800

    t543 二叉树的直径

---
+++ Java/leetcode/src/hot100/t543diameterOfBinaryTree.java
@@ -0,0 +1,61 @@
+package hot100;
+
+public class t543diameterOfBinaryTree {
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode() {}
+     *     TreeNode(int val) { this.val = val; }
+     *     TreeNode(int val, TreeNode left, TreeNode right) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
+     */
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+
+    class Solution {
+        int ans;
+        public int diameterOfBinaryTree(TreeNode root) {
+            ans = 1;
+            depth(root);
+            return ans - 1;
+        }
+        public int depth(TreeNode node) {
+            if (node == null) {
+                return 0; // 访问到空节点了，返回0
+            }
+            int L = depth(node.left); // 左儿子为根的子树的深度
+            int R = depth(node.right); // 右儿子为根的子树的深度
+            ans = Math.max(ans, L+R+1); // 计算d_node即L+R+1 并更新ans
+            return Math.max(L, R) + 1; // 返回该节点为根的子树的深度
+        }
+    }
+
+    // public static void main(String[] args) {
+    //     Solution sol = new Solution();
+    //     TreeNode root = new TreeNode(1);
+    //     root.left = new TreeNode(2);
+    //     root.right = new TreeNode(3);
+    //     root.left.left = new TreeNode(4);
+    //     root.left.right = new TreeNode(5);
+    //
+    //     System.out.println(sol.diameterOfBinaryTree(root));
+    // }
+}
