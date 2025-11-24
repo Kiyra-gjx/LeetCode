8d8d4fc5377a544c739e6e1f035738106771e594
Kiyra <Kiyra.gjx@gmail.com>
2025-11-24 16:37:09 +0800

    t230 二叉搜索树的第k小元素 方法一：记录子树的节点数

--- Java/leetcode/src/hot100/t230kthSmallest.java
+++ Java/leetcode/src/hot100/t230kthSmallest.java
@@ -1,5 +1,8 @@
 package hot100;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class t230kthSmallest {
     /**
      * Definition for a binary tree node.
@@ -30,20 +33,46 @@ public class t230kthSmallest {
     }
 
     class Solution {
-        int res, k;
+        public int kthSmallest(TreeNode root, int k) {
+            MyBst bst = new MyBst(root);
+            return bst.kthSmallest(k);
+        }
+    }
 
-        void dfs(TreeNode root) {
-            if (root == null) return;
-            dfs(root.left);
-            if (k == 0) return;
-            if (--k == 0) this.res = root.val;
-            dfs(root.right);
+    class MyBst {
+        TreeNode root;
+        Map<TreeNode, Integer> nodeNum;
+
+        public MyBst(TreeNode root) {
+            this.root = root;
+            this.nodeNum = new HashMap<TreeNode, Integer>();
+            countNodeNum(root);
         }
 
-        public int kthSmallest(TreeNode root, int k) {
-            this.k = k;
-            dfs(root);
-            return this.res;
+        public int kthSmallest(int k) {
+            TreeNode node = root;
+            while(node != null) {
+                int left = getNodeNum(node.left);
+                if (left < k - 1) {
+                    node = node.right;
+                    k -= left + 1;
+                } else if (left == k - 1) {
+                    break;
+                } else {
+                    node = node.left;
+                }
+            }
+            return node.val;
+        }
+
+        private int countNodeNum(TreeNode node) {
+            if (node == null) return 0;
+            nodeNum.put(node, 1 + countNodeNum(node.left) + countNodeNum(node.right));
+            return nodeNum.get(node);
+        }
+
+        private int getNodeNum(TreeNode node) {
+            return nodeNum.getOrDefault(node, 0);
         }
     }
 }
