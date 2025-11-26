2e25194f668ba9694e2718bf2434f226a38883de
Kiyra <Kiyra.gjx@gmail.com>
2025-11-26 21:45:08 +0800

    t200 岛屿数量 方法二：dfs

--- Java/leetcode/src/hot100/t200numIslands.java
+++ Java/leetcode/src/hot100/t200numIslands.java
@@ -5,40 +5,39 @@ import java.util.Queue;
 
 public class t200numIslands {
     class Solution {
+        void dfs(char[][] grid, int r, int c) {
+            int nr = grid.length;
+            int nc = grid[0].length;
+
+            if (r < 0 || c < 0 || r >= nr || c >= nc || grid[r][c] == '0') {
+                return;
+            }
+
+            grid[r][c] = '0';
+            dfs(grid, r - 1, c);
+            dfs(grid, r + 1, c);
+            dfs(grid, r, c - 1);
+            dfs(grid, r, c + 1);
+        }
+
         public int numIslands(char[][] grid) {
-            if (grid == null || grid.length == 0) return 0;
+            if (grid == null || grid.length == 0) {
+                return 0;
+            }
 
-            int count = 0;
-            int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-
-            for (int i = 0; i < grid.length; i++) {
-                for (int j = 0; j < grid[0].length; j++) {
-                    if (grid[i][j] == '1') {
-                        count++;
-                        Queue<int[]> queue = new LinkedList<>();
-                        queue.offer(new int[]{i, j});
-                        grid[i][j] = '0';
-
-                        while (!queue.isEmpty()) {
-                            int[] cell = queue.poll();
-                            int x = cell[0], y = cell[1];
-
-                            for (int[] dir : directions) {
-                                int newX = x + dir[0];
-                                int newY = y + dir[1];
-
-                                if (newX >= 0 && newX < grid.length &&
-                                        newY >= 0 && newY < grid[0].length &&
-                                        grid[newX][newY] == '1') {
-                                    queue.offer(new int[]{newX, newY});
-                                    grid[newX][newY] = '0';
-                                }
-                            }
-                        }
+            int nr = grid.length;
+            int nc = grid[0].length;
+            int num_islands = 0;
+            for (int r = 0; r < nr; ++r) {
+                for (int c = 0; c < nc; ++c) {
+                    if (grid[r][c] == '1') {
+                        ++num_islands;
+                        dfs(grid, r, c);
                     }
                 }
             }
-            return count;
+
+            return num_islands;
         }
     }
 
