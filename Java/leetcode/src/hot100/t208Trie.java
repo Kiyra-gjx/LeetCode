bd449b1bff60697331b761cabf0c3d1e8690e315
Kiyra <Kiyra.gjx@gmail.com>
2025-11-29 13:32:15 +0800

    t208 字典树

---
+++ Java/leetcode/src/hot100/t208Trie.java
@@ -0,0 +1,56 @@
+package hot100;
+
+public class t208Trie {
+    class Trie {
+        private Trie[] children;
+        private boolean isEnd;
+
+        public Trie() {
+            children = new Trie[26];
+            isEnd = false;
+        }
+
+        public void insert(String word) {
+            Trie node = this;
+            for (int i = 0; i < word.length(); i++) {
+                char c = word.charAt(i);
+                int idx = c - 'a';
+                if (node.children[idx] == null) {
+                    node.children[idx] = new Trie();
+                }
+                node = node.children[idx];
+            }
+            node.isEnd = true;
+        }
+
+        public boolean search(String word) {
+            Trie node = searchPrefix(word);
+            return node != null && node.isEnd;
+        }
+
+        public boolean startsWith(String prefix) {
+            return searchPrefix(prefix) != null;
+        }
+
+        private Trie searchPrefix(String prefix) {
+            Trie node = this;
+            for (int i = 0; i < prefix.length(); i++) {
+                char ch = prefix.charAt(i);
+                int idx = ch - 'a';
+                if (node.children[idx] == null) {
+                    return null;
+                }
+                node = node.children[idx];
+            }
+            return node;
+        }
+    }
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
+}
