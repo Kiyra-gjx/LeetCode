5c9d0c48828c01ece62d125b59c08a11c3e6c992
Kiyra <Kiyra.gjx@gmail.com>
2025-12-01 12:54:14 +0800

    t51 n皇后 解法二：利用位运算

--- Java/leetcode/src/hot100/t51solveNQueens.java
+++ Java/leetcode/src/hot100/t51solveNQueens.java
@@ -8,39 +8,23 @@ public class t51solveNQueens {
             List<List<String>> ans = new ArrayList<>();
             int[] queens = new int[n];
             Arrays.fill(queens, -1);
-            Set<Integer> columns = new HashSet<>();
-            Set<Integer> diagonals1 = new HashSet<>();
-            Set<Integer> diagonals2 = new HashSet<>();
-            dfs(ans, queens, n, 0, columns, diagonals1, diagonals2);
+            dfs(ans, queens, n, 0, 0, 0, 0);
             return ans;
         }
 
-        void dfs(List<List<String>> ans, int[] queens, int n, int row, Set<Integer> columns, Set<Integer> diagonals1, Set<Integer> diagonals2) {
+        void dfs(List<List<String>> ans, int[] queens, int n, int row, int columns, int diagonals1, int diagonals2) {
             if (row == n) {
                 List<String> board = generateBoard(queens, n);
                 ans.add(board);
             } else {
-                for (int i = 0; i < n; i++) {
-                    if (columns.contains(i)) {
-                        continue;
-                    }
-                    int diagonal1 = row - i;
-                    if (diagonals1.contains(diagonal1)) {
-                        continue;
-                    }
-                    int diagonal2 = row + i;
-                    if (diagonals2.contains(diagonal2)) {
-                        continue;
-                    }
-                    queens[row] = i;
-                    columns.add(i);
-                    diagonals1.add(diagonal1);
-                    diagonals2.add(diagonal2);
-                    dfs(ans, queens, n, row + 1, columns, diagonals1, diagonals2);
+                int availablePositions = ((1 << n) - 1) & (~(columns | diagonals1 | diagonals2));
+                while(availablePositions != 0) {
+                    int position = availablePositions & (-availablePositions);
+                    availablePositions = availablePositions & (availablePositions - 1);
+                    int column = Integer.bitCount(position - 1);
+                    queens[row] = column;
+                    dfs(ans, queens, n, row + 1, columns | position, (diagonals1 | position) << 1, (diagonals2 | position) >> 1);
                     queens[row] = -1;
-                    columns.remove(i);
-                    diagonals1.remove(diagonal1);
-                    diagonals2.remove(diagonal2);
                 }
             }
         }
