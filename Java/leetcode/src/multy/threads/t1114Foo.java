1ce4177d939cddcb9fbae8e777c069b60f8ec840
Kiyra <Kiyra.gjx@gmail.com>
2025-12-05 15:06:25 +0800

    t1114 按序打印 方法四：ReentrantLock + Condition

--- Java/leetcode/src/multy/threads/t1114Foo.java
+++ Java/leetcode/src/multy/threads/t1114Foo.java
@@ -1,6 +1,10 @@
 package multy.threads;
 
+import java.nio.FloatBuffer;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class t1114Foo {
 
@@ -8,34 +12,62 @@ public class t1114Foo {
 
 class Foo {
 
-    private CountDownLatch countDownLatch = new CountDownLatch(3);
+    private Lock lock = new ReentrantLock();
+    private Condition condition = lock.newCondition();
+    private int state = 1;
 
     public Foo() {
 
     }
 
     public void first(Runnable printFirst) throws InterruptedException {
-
-        // printFirst.run() outputs "first". Do not change or remove this line.
-        printFirst.run();
-
-        countDownLatch.countDown();
+        lock.lock();
+        try {
+            while (state != 1) {
+                condition.await();
+            }
+            // printFirst.run() outputs "first". Do not change or remove this line.
+            printFirst.run();
+            state++;
+            condition.signalAll();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            lock.unlock();
+        }
     }
 
     public void second(Runnable printSecond) throws InterruptedException {
-        while(countDownLatch.getCount() != 2);
-
-        // printSecond.run() outputs "second". Do not change or remove this line.
-        printSecond.run();
-
-        countDownLatch.countDown();
+        lock.lock();
+        try {
+            while (state != 2) {
+                condition.await();
+            }
+            // printSecond.run() outputs "second". Do not change or remove this line.
+            printSecond.run();
+            state++;
+            condition.signalAll();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            lock.unlock();
+        }
     }
 
     public void third(Runnable printThird) throws InterruptedException {
-        while(countDownLatch.getCount() != 1);
-
-        // printThird.run() outputs "third". Do not change or remove this line.
-        printThird.run();
-        countDownLatch.countDown();
+        lock.lock();
+        try {
+            while(state != 3) {
+                condition.await();
+            }
+            // printThird.run() outputs "third". Do not change or remove this line.
+            printThird.run();
+            state++;
+            condition.signalAll();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            lock.unlock();
+        }
     }
 }
