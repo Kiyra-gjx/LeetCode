8da1739ae18ed53794c097ac97cf884261a3ab42
Kiyra <Kiyra.gjx@gmail.com>
2025-12-05 14:59:52 +0800

    t1114 按序打印 方法三：CountDownLatch

--- Java/leetcode/src/multy/threads/t1114Foo.java
+++ Java/leetcode/src/multy/threads/t1114Foo.java
@@ -1,6 +1,6 @@
 package multy.threads;
 
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.CountDownLatch;
 
 public class t1114Foo {
 
@@ -8,7 +8,7 @@ public class t1114Foo {
 
 class Foo {
 
-    private volatile int count = 0;
+    private CountDownLatch countDownLatch = new CountDownLatch(3);
 
     public Foo() {
 
@@ -19,26 +19,23 @@ class Foo {
         // printFirst.run() outputs "first". Do not change or remove this line.
         printFirst.run();
 
-        count++;
+        countDownLatch.countDown();
     }
 
     public void second(Runnable printSecond) throws InterruptedException {
-        while(count != 1) {
-
-        }
+        while(countDownLatch.getCount() != 2);
 
         // printSecond.run() outputs "second". Do not change or remove this line.
         printSecond.run();
 
-        count++;
+        countDownLatch.countDown();
     }
 
     public void third(Runnable printThird) throws InterruptedException {
-        while(count != 2) {
-
-        }
+        while(countDownLatch.getCount() != 1);
 
         // printThird.run() outputs "third". Do not change or remove this line.
         printThird.run();
+        countDownLatch.countDown();
     }
 }
