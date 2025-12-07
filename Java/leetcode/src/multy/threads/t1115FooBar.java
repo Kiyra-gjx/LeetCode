8551f26a20c2b562dafc4f9e36395dfe0e5966e4
Kiyra <Kiyra.gjx@gmail.com>
2025-12-07 14:30:17 +0800

    t1115 交替打印FooBar 方法六：信号量 适合控制顺序

--- Java/leetcode/src/multy/threads/t1115FooBar.java
+++ Java/leetcode/src/multy/threads/t1115FooBar.java
@@ -1,45 +1,35 @@
 package multy.threads;
 
 import java.util.Objects;
+import java.util.concurrent.Semaphore;
 
 public class t1115FooBar {
 }
 
 class FooBar {
     private int n;
+    private Semaphore foo = new Semaphore(1);
+    private Semaphore bar = new Semaphore(0);
 
     public FooBar(int n) {
         this.n = n;
     }
 
-    private volatile boolean type = true;
-    private final Object foo = new Object();
-
     public void foo(Runnable printFoo) throws InterruptedException {
 
         for (int i = 0; i < n; i++) {
-            synchronized (foo) {
-                while(!type) {
-                    foo.wait();
-                }
-                printFoo.run();
-                type = false;
-                foo.notify();
-            }
+            foo.acquire();
+            printFoo.run();
+            bar.release();
         }
     }
 
     public void bar(Runnable printBar) throws InterruptedException {
 
         for (int i = 0; i < n; i++) {
-            synchronized (foo) {
-                while (type) {
-                    foo.wait();
-                }
-                printBar.run();
-                type = true;
-                foo.notify();
-            }
+            bar.acquire();
+            printBar.run();
+            foo.release();
         }
     }
 }
