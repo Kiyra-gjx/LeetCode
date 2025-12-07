36b12c97076a593ff42fe1dbed3bcb530ec074c4
Kiyra <Kiyra.gjx@gmail.com>
2025-12-07 14:09:25 +0800

    t1115 交替打印FooBar 方法三：自旋 + 让出CPU

--- Java/leetcode/src/multy/threads/t1115FooBar.java
+++ Java/leetcode/src/multy/threads/t1115FooBar.java
@@ -1,10 +1,5 @@
 package multy.threads;
 
-import java.util.concurrent.BlockingDeque;
-import java.util.concurrent.BrokenBarrierException;
-import java.util.concurrent.CyclicBarrier;
-import java.util.concurrent.LinkedBlockingDeque;
-
 public class t1115FooBar {
 }
 
@@ -15,31 +10,31 @@ class FooBar {
         this.n = n;
     }
 
-    CyclicBarrier cb = new CyclicBarrier(2);
-    volatile boolean fi = true;
+    volatile boolean permitFoo = true;
 
     public void foo(Runnable printFoo) throws InterruptedException {
 
-        for (int i = 0; i < n; i++) {
-            while(!fi);
-            printFoo.run();
-            fi = false;
-            try {
-                cb.await();
-            } catch (BrokenBarrierException e) {
+        for (int i = 0; i < n;) {
+            if (permitFoo) {
+                printFoo.run();
+                i++;
+                permitFoo = false;
+            } else {
+                Thread.yield();
             }
         }
     }
 
     public void bar(Runnable printBar) throws InterruptedException {
 
-        for (int i = 0; i < n; i++) {
-            try {
-                cb.await();
-            } catch (BrokenBarrierException e) {
+        for (int i = 0; i < n;) {
+            if (!permitFoo) {
+                printBar.run();
+                i++;
+                permitFoo = true;
+            } else {
+                Thread.yield();
             }
-            printBar.run();
-            fi = true;
         }
     }
 }
