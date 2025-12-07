9dd3d1e226cc11d89018196e7467eb0a7e54f5d0
Kiyra <Kiyra.gjx@gmail.com>
2025-12-07 14:03:31 +0800

    t1115 交替打印FooBar 方法二：CyclicBarrier 控制先后

--- Java/leetcode/src/multy/threads/t1115FooBar.java
+++ Java/leetcode/src/multy/threads/t1115FooBar.java
@@ -1,6 +1,8 @@
 package multy.threads;
 
 import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.LinkedBlockingDeque;
 
 public class t1115FooBar {
@@ -8,30 +10,36 @@ public class t1115FooBar {
 
 class FooBar {
     private int n;
-    private BlockingDeque<Integer> bar = new LinkedBlockingDeque<>(1);
-    private BlockingDeque<Integer> foo = new LinkedBlockingDeque<>(1);
 
     public FooBar(int n) {
         this.n = n;
     }
 
+    CyclicBarrier cb = new CyclicBarrier(2);
+    volatile boolean fi = true;
+
     public void foo(Runnable printFoo) throws InterruptedException {
 
         for (int i = 0; i < n; i++) {
-            // printFoo.run() outputs "foo". Do not change or remove this line.
-            foo.put(i);
+            while(!fi);
             printFoo.run();
-            bar.put(i);
+            fi = false;
+            try {
+                cb.await();
+            } catch (BrokenBarrierException e) {
+            }
         }
     }
 
     public void bar(Runnable printBar) throws InterruptedException {
 
         for (int i = 0; i < n; i++) {
-            // printBar.run() outputs "bar". Do not change or remove this line.
-            bar.take();
+            try {
+                cb.await();
+            } catch (BrokenBarrierException e) {
+            }
             printBar.run();
-            foo.take();
+            fi = true;
         }
     }
 }
