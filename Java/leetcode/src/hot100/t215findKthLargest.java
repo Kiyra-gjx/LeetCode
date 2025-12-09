814a864c80cb91dc98acb2025d60bb14ff62a602
Kiyra <Kiyra.gjx@gmail.com>
2025-12-09 13:51:30 +0800

    t215 数组中的第k个最大元素 手写快排

--- Java/leetcode/src/hot100/t215findKthLargest.java
+++ Java/leetcode/src/hot100/t215findKthLargest.java
@@ -1,14 +1,26 @@
 package hot100;
 
-import java.util.Arrays;
-import java.util.PriorityQueue;
-
 public class t215findKthLargest {
 }
 
 class Solution {
+    int quickselect(int[] nums, int left, int right, int k) {
+        if (left == right) return nums[k];
+        int x = nums[left], i = left - 1, j = right + 1;
+        while (i < j) {
+            do i++; while (nums[i] < x);
+            do j--; while (nums[j] > x);
+            if (i < j){
+                int tmp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = tmp;
+            }
+        }
+        if (k <= j) return quickselect(nums, left, j, k);
+        else return quickselect(nums, j + 1, right, k);
+    }
     public int findKthLargest(int[] nums, int k) {
-        Arrays.sort(nums);
-        return nums[nums.length - k];
+        int n = nums.length;
+        return quickselect(nums, 0, n - 1, n - k);
     }
 }
