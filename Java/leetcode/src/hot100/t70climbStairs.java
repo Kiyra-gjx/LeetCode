af16b7038dc75589e38ba6a2e72a142a76055a62
Kiyra <Kiyra.gjx@gmail.com>
2025-12-11 12:43:06 +0800

    t70 爬楼梯

---
+++ Java/leetcode/src/hot100/t70climbStairs.java
@@ -0,0 +1,15 @@
+package hot100;
+
+public class t70climbStairs {
+}
+
+class t70Solution {
+    public int climbStairs(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+}
