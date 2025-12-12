1f1e1b515d1af59e059e51c7d2c9a747556c8eea
Kiyra <Kiyra.gjx@gmail.com>
2025-12-12 13:33:57 +0800

    t198 打家劫舍

---
+++ Java/leetcode/src/hot100/t198rob.java
@@ -0,0 +1,16 @@
+package hot100;
+
+public class t198rob {
+}
+
+class t198Solution {
+    public int rob(int[] nums) {
+        int[][] dp = new int[nums.length][2];
+        dp[0][1] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            dp[i][1] = dp[i - 1][0] + nums[i];
+            dp[i][0] = Math.max(dp[i - 1][1], dp[i - 1][0]);
+        }
+        return Math.max(dp[nums.length - 1][1], dp[nums.length - 1][0]);
+    }
+}
