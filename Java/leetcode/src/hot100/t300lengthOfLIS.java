92cd30dc6bc45b4c96a4d52e59a58ddafa8ad565
Kiyra <Kiyra.gjx@gmail.com>
2025-12-14 13:49:23 +0800

    t300 最长递增子序列 解法二：O(nlog(n))

--- Java/leetcode/src/hot100/t300lengthOfLIS.java
+++ Java/leetcode/src/hot100/t300lengthOfLIS.java
@@ -7,16 +7,30 @@ public class t300lengthOfLIS {
 
 class t300Solution {
     public int lengthOfLIS(int[] nums) {
-        int[] dp = new int[nums.length];
-        Arrays.fill(nums, 1);
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j < i; j++) {
-                if (nums[i] > nums[j]) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
+        int[] d = new int[nums.length + 1];
+        d[1] = nums[0];
+        int len = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == d[len]) {
+                continue;
+            } else if (nums[i] > d[len]) {
+                d[++len] = nums[i];
+            } else {
+                int left = 1, right = len;
+                int idx = len;
+                while(left <= right) {
+                    int mid = (left + right) / 2;
+                    if (d[mid] >= nums[i]) {
+                        idx = mid;
+                        right = mid - 1;
+                    } else {
+                        left = mid + 1;
+                    }
                 }
+                d[idx] = nums[i];
             }
         }
 
-        return dp[nums.length - 1];
+        return len;
     }
 }
