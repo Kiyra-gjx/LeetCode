6c5e53d81e8ddd66a4fd23d843aac537bfc58fd1
Kiyra <Kiyra.gjx@gmail.com>
2025-12-17 13:43:46 +0800

    t32 最长有效括号 解法二：动态规划

--- Java/leetcode/src/hot100/t32longestValidParentheses.java
+++ Java/leetcode/src/hot100/t32longestValidParentheses.java
@@ -8,20 +8,16 @@ public class t32longestValidParentheses {
 
 class t32Solution {
     public int longestValidParentheses(String s) {
-        // int inValid = 0;
-        Deque<Integer> stack = new ArrayDeque<>();
-        stack.push(-1);
+        int[] dp = new int[s.length()];
         int ans = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(') {
-                stack.push(i);
-            } else {
-                stack.pop();
-                if (stack.isEmpty()) {
-                    stack.push(i);
-                } else {
-                    ans = Math.max(ans, i - stack.peek());
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == ')') {
+                if (s.charAt(i - 1) == '(') {
+                    dp[i] = (i >= 2 ? dp[i - 2] : 0) + 2;
+                } else if (i - dp[i - 1] > 0 && s.charAt(i - dp[i - 1] - 1) == '(') {
+                    dp[i] = dp[i - 1] + (i - dp[i - 1] - 2 >= 0 ? dp[i - dp[i - 1] - 2] : 0) + 2;
                 }
+                ans = Math.max(ans, dp[i]);
             }
         }
 
